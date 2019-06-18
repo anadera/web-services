1772c2c37156b7a1f81189ddc3da51b311663f60
Anzhelika Deriabina <hey_biscuit@mail.ru>
2019-06-19 00:07:30 +0300

    initial commit lab1

---
+++ lab1/src/main/java/ifmo/web/lab1/Item.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ifmo.web.lab1;
+
+/**
+ *
+ * @author Anzhelika
+ */
+public class Item {
+    
+}
